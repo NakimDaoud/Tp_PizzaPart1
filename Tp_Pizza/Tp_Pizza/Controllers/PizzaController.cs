d4002bd6b84253436b441d49f477777679e21a75
Nakim Daoud <nakim.daoud2018@campus-eni.fr>
2020-03-20 10:53:43 +0100

    pizzaaaaaaa

---
+++ Tp_Pizza/Tp_Pizza/Controllers/PizzaController.cs
@@ -0,0 +1,155 @@
+﻿using BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tp_Pizza.Models;
+using Tp_Pizza.Utils;
+
+namespace Tp_Pizza.Controllers
+{
+    public class PizzaController : Controller
+    {
+
+
+
+        
+        // GET: Pizza
+        public ActionResult Index()
+        {
+            return View(FakeDB.Instance.Pizzas);
+        }
+
+        // GET: Pizza/Details/5
+        public ActionResult Details(int id)
+        {
+            var pizza = FakeDB.Instance.Pizzas.FirstOrDefault(p => p.Id == id); if (pizza != null) { return View(pizza); }
+
+            return RedirectToAction("Index");
+        }
+
+        // GET: Pizza/Create
+        public ActionResult Create()
+        {
+            PizzaViewModel vm = new PizzaViewModel();
+
+            vm.Pates = FakeDB.Instance.patesDispo.Select(
+                x => new SelectListItem { Text = x.Nom, Value = x.Id.ToString() })
+                .ToList();
+
+            vm.Ingredients = FakeDB.Instance.ingredientDispo.Select(
+                x => new SelectListItem { Text = x.Nom, Value = x.Id.ToString() })
+                .ToList();
+
+            return View(vm);
+        }
+
+        // POST: Pizza/Create
+        [HttpPost]
+        public ActionResult Create(PizzaViewModel vm)
+        {
+            try
+            {
+                Pizza pizza = vm.Pizza;
+
+                pizza.Pate = FakeDB.Instance.patesDispo.FirstOrDefault(x => x.Id == vm.IdPate);
+
+                pizza.Ingredients = FakeDB.Instance.ingredientDispo.Where(
+                    x => vm.IdsIngredients.Contains(x.Id))
+                    .ToList();
+
+                // Insuffisant
+                //pizza.Id = FakeDb.Instance.Pizzas.Count + 1;
+
+                pizza.Id = FakeDB.Instance.Pizzas.Count == 0 ? 1 : FakeDB.Instance.Pizzas.Max(x => x.Id) + 1;
+
+                FakeDB.Instance.Pizzas.Add(pizza);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Pizza/Edit/5
+        public ActionResult Edit(int id)
+        {
+
+            PizzaViewModel vm = new PizzaViewModel();
+
+            vm.Pates = FakeDB.Instance.patesDispo.Select(
+                x => new SelectListItem { Text = x.Nom, Value = x.Id.ToString() })
+                .ToList();
+
+            vm.Ingredients = FakeDB.Instance.ingredientDispo.Select(
+                x => new SelectListItem { Text = x.Nom, Value = x.Id.ToString() })
+                .ToList();
+
+            vm.Pizza = FakeDB.Instance.Pizzas.FirstOrDefault(x => x.Id == id);
+
+            if (vm.Pizza.Pate != null) {
+                vm.IdPate = vm.Pizza.Pate.Id;
+            }
+
+            if (vm.Pizza.Ingredients.Any())
+            {
+                vm.IdsIngredients = vm.Pizza.Ingredients.Select(x => x.Id).ToList();
+            }
+
+            return View(vm);
+        }
+
+        // POST: Pizza/Edit/5
+        [HttpPost]
+        public ActionResult Edit(PizzaViewModel vm)
+        {
+            try
+            {
+                Pizza pizza = FakeDB.Instance.Pizzas.FirstOrDefault(p => p.Id == vm.Pizza.Id);
+                pizza.Nom = vm.Pizza.Nom;
+                pizza.Pate = FakeDB.Instance.patesDispo.FirstOrDefault(x => x.Id == vm.IdPate);
+
+                pizza.Ingredients = FakeDB.Instance.ingredientDispo.Where(
+                    x => vm.IdsIngredients.Contains(x.Id))
+                    .ToList();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Pizza/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var pizza = FakeDB.Instance.Pizzas.FirstOrDefault(p => p.Id == id); if (pizza != null) { return View(pizza); }
+
+            return RedirectToAction("Index");
+        }
+
+        // POST: Pizza/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                var pizza = FakeDB.Instance.Pizzas.FirstOrDefault(p => p.Id == id); if (pizza != null) {
+                    FakeDB.Instance.Pizzas.Remove(pizza);
+                   }
+
+                
+            }
+            catch
+            {
+                return RedirectToAction("Index");
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Tp_Pizza/Tp_Pizza/Utils/FakeDB.cs
@@ -0,0 +1,147 @@
+﻿using BO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tp_Pizza.Utils
+{
+    public class FakeDB
+    {
+
+
+        private static FakeDB _instance;
+        static readonly object instanceLock = new object();
+
+        private FakeDB() {
+
+            pizzas = this.getListePizzas();
+            initIngredient();
+            initPate();
+        }
+
+        public static FakeDB Instance
+        {
+            get
+            {
+                if (_instance == null) //Les locks prennent du temps, il est préférable de vérifier d'abord la nullité de l'instance.
+                {
+                    lock (instanceLock)
+                    {
+                        if (_instance == null) //on vérifie encore, au cas où l'instance aurait été créée entretemps.
+                            _instance = new FakeDB();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        private List<Pizza> pizzas;
+
+        public List<Pizza> Pizzas
+        {
+            get { return pizzas; }
+        }
+
+        private List<Pizza> getListePizzas() {
+            pizzas = new List<Pizza>
+            {
+                new Pizza{Id=1,
+
+                    Ingredients = new List<Ingredient>
+                    {
+                        Pizza.IngredientsDisponibles.First(i => i.Id == 1),
+                        Pizza.IngredientsDisponibles.First(i => i.Id == 2),
+                        Pizza.IngredientsDisponibles.First(i => i.Id == 4),
+                        Pizza.IngredientsDisponibles.First(i => i.Id == 6),
+                    },
+                    Nom="Pizza 1",
+                    Pate=Pizza.PatesDisponibles.First(p=>p.Id == 1)
+
+
+
+                    },
+
+                new Pizza{Id=2,
+
+                    Ingredients = new List<Ingredient>
+                    {
+                        Pizza.IngredientsDisponibles.First(i => i.Id == 2),
+                        Pizza.IngredientsDisponibles.First(i => i.Id == 4),
+                        Pizza.IngredientsDisponibles.First(i => i.Id == 6),
+                        Pizza.IngredientsDisponibles.First(i => i.Id == 8),
+                    },
+                    Nom="Pizza 2",
+                    Pate=Pizza.PatesDisponibles.First(p=>p.Id == 2)
+
+
+
+                    },
+
+                new Pizza{Id=3,
+
+                    Ingredients = new List<Ingredient>
+                    {
+                        Pizza.IngredientsDisponibles.First(i => i.Id == 1),
+                        Pizza.IngredientsDisponibles.First(i => i.Id == 3),
+                        Pizza.IngredientsDisponibles.First(i => i.Id == 5),
+                        Pizza.IngredientsDisponibles.First(i => i.Id == 7),
+                    },
+                    Nom="Pizza 3",
+                    Pate=Pizza.PatesDisponibles.First(p=>p.Id == 3)
+
+
+
+                    },
+
+                new Pizza{Id=4,
+
+                    Ingredients = new List<Ingredient>
+                    {
+                        Pizza.IngredientsDisponibles.First(i => i.Id == 1),
+                        Pizza.IngredientsDisponibles.First(i => i.Id == 2),
+                        Pizza.IngredientsDisponibles.First(i => i.Id == 5),
+                        Pizza.IngredientsDisponibles.First(i => i.Id == 8),
+                    },
+                    Nom="Pizza 4",
+                    Pate=Pizza.PatesDisponibles.First(p=>p.Id == 4)
+
+
+
+                    }
+            };
+
+            return Pizzas;
+        }
+
+        private List<Ingredient> IngredientDispo;
+
+        public List<Ingredient> ingredientDispo
+        {
+            get { return IngredientDispo; }
+           
+        }
+
+        private List<Pate> PatesDispo;
+
+        public List<Pate> patesDispo
+        {
+            get { return PatesDispo; }
+        }
+
+        private void initIngredient()
+        {
+            IngredientDispo = BO.Pizza.IngredientsDisponibles;
+        }
+
+        private void initPate()
+        {
+            PatesDispo = BO.Pizza.PatesDisponibles;
+        }
+
+        public void addPizza(Pizza pizza) {
+            pizzas.Add(pizza);
+        }
+
+    }
+}
